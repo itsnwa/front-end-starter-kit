9a4799646c381e16d69386d20251d473c9753d35
itsnwa <nichlas@itsnwa.com>
2016-09-21 18:16:56 -0400

    Added px to rem postcss plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
    postcss = require('gulp-postcss'),
    cssnext = require('postcss-cssnext'),
+   pxtorem = require('postcss-pxtorem'),
 sourcemaps = require('gulp-sourcemaps'),
    cssnano = require('gulp-cssnano');
 
@@ -10,6 +11,10 @@ gulp.task('css', function () {
   var processors = [
     cssnext({
       browsers: ['last 4 version']
+    }),
+    pxtorem({
+      propWhiteList: [],
+      mediaQuery: true
     })
   ];
 
