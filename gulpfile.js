99772d2cf224352fc347858db545e89df1352474
itsnwa <nichlas@itsnwa.com>
2016-09-21 17:38:16 -0400

    Added css minification with sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const gulp = require('gulp'),
    postcss = require('gulp-postcss'),
    cssnext = require('postcss-cssnext'),
-sourcemaps = require('gulp-sourcemaps');
+sourcemaps = require('gulp-sourcemaps'),
+   cssnano = require('gulp-cssnano');
 
 
 gulp.task('css', function () {
 
   var processors = [
-    cssnext({browsers: ['last 4 version']})
+    cssnext({
+      browsers: ['last 4 version']
+    })
   ];
 
   return gulp.src('./src/*.css')
   .pipe(sourcemaps.init())
     .pipe(postcss(processors))
+    .pipe(cssnano())
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('./dest'));
 
