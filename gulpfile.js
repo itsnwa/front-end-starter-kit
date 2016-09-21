4a45740a3f2bc7314755801fd7fd1ecc8d97e1ea
itsnwa <nichlas@itsnwa.com>
2016-09-21 16:38:00 -0400

    Added sourcemaps to gulp flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
 const gulp = require('gulp'),
    postcss = require('gulp-postcss'),
-   cssnext = require('postcss-cssnext');
+   cssnext = require('postcss-cssnext'),
+sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('css', function () {
+
+  var processors = [
+    cssnext({browsers: ['last 4 version']})
+  ];
+
+  return gulp.src('./src/*.css')
+  .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./dest'));
+
+});
