2efea2252e0063cae7ae7e3178a30eb412920476
itsnwa <nichlas@itsnwa.com>
2016-09-25 00:08:00 -0400

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -23,18 +23,17 @@ const gulp        = require('gulp'),
 
 
       // Default source and build folder
-var   input       = './src',
-      output      = './build';
+var   src         = './src',
+      dist        = './assets';
 
 
-// Folders
+// Folders ( Make sure you add new jekyll folders to the jekyll line as needed )
 let path = {
-      html: [input + '/**/*.html'],
-      css: [input + '/css/**/*.css'],
-      js: [input + '/js/**/*.js'],
-      images: [input + '/images/**/**/*'],
-      fonts: [input + '/fonts/**/*'],
-      jekyll: ['index.html', '_layouts', '_site', '_includes', '_data']
+      css: [src + '/css/**/*.css'],
+      js: [src + '/js/**/*.js'],
+      images: [src + '/images/**/**/*'],
+      fonts: [src + '/fonts/**/*'],
+      jekyll: ['index.html', '_layouts', '_includes', '_data']
     };
 
 
@@ -44,7 +43,7 @@ var messages = {
 };
 
 // Build the Jekyll Site
-gulp.task('jekyll-build', function (code) {
+gulp.task('jekyll-build', (code) => {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
       .on('error', (error) => gutil.log(gutil.colors.red(error.message)))
@@ -53,7 +52,7 @@ gulp.task('jekyll-build', function (code) {
 
 
 // Rebuild Jekyll & do page reload
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', ['jekyll-build'], () => {
     browserSync.reload();
 });
 
@@ -73,14 +72,12 @@ gulp.task('css', () => {
     })
   ];
 
-  return gulp.src(input + '/css/main.css')
+  return gulp.src(src + '/css/main.css')
   .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .pipe(cssnano())
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest(output + '/css/'))
-  .pipe(browserSync.stream());
-
+  .pipe(gulp.dest(dist + '/css/'))
 });
 
 
@@ -88,98 +85,90 @@ gulp.task('css', () => {
 gulp.task('js', () => {
   return gulp.src(path.js)
     .pipe(uglify())
-    .pipe(gulp.dest(output + '/js/'));
+    .pipe(gulp.dest(dist + '/js/'))
 });
 
 
 // Images
 gulp.task('imagemin', () => {
 	return gulp.src(path.images)
-		.pipe(imagemin())
-    .pipe(gulp.dest(output + '/images/'))
-    .pipe(browserSync.stream());
+	  .pipe(imagemin())
+    .pipe(gulp.dest(dist + '/images/'))
 });
 
 
-// HTML
-gulp.task('html', () => {
-  return gulp.src(path.html)
-  .pipe(htmlmin({
-    removeComments: true,
-    collapseWhitespace: true,
-    collapseBooleanAttributes: true,
-    removeAttributeQuotes: true,
-    removeRedundantAttributes: true,
-    removeEmptyAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    removeOptionalTags: true
-  }))
-  .pipe(gulp.dest(output))
-  .pipe(browserSync.stream());
+// Copy font files
+gulp.task('copy-fonts', () => {
+  gulp.src(path.fonts)
+  .pipe(gulp.dest(dist + '/fonts/'));
 });
 
 
 // Clean build folder
-gulp.task('clean', () => {
-  return del(output);
-});
+gulp.task('clean', () => del(['_site'], {dot: true}));
+gulp.task('clean:all', () => del(['_site', 'assets'], {dot: true}));
 
 
 // Server
-gulp.task('server', ['css', 'jekyll-build'], () => {
-    browserSync.init({
-        server: {
-            baseDir: output
-        },
-        notify: {
-            styles: {
-                top: '30px',
-                left: '50%',
-                width: '200px',
-                transform: 'translate(-50%,-50%)',
-                margin: '0px',
-                padding: '8px 12px',
-                position: 'fixed',
-                fontSize: '13px',
-                fontFamily: 'Helvetica',
-                zIndex: '9999',
-                borderRadius: '16px',
-                color: 'white',
-                textAlign: 'center',
-                display: 'block',
-                backgroundColor: 'rgba(0,0,0, 0.8)'
-            }
-        },
-        logLevel: 'silent',
-        browser: 'google chrome'
-    });
+gulp.task('server', ['jekyll-build'], () => {
+  browserSync.init({
+    server: {
+      baseDir: '_site',
+    },
+    port: 3000,
+    notify: {
+      styles: {
+        top: '30px',
+        left: '50%',
+        width: '200px',
+        transform: 'translate(-50%,-50%)',
+        margin: '0px',
+        padding: '8px 12px',
+        position: 'fixed',
+        fontSize: '13px',
+        fontFamily: 'Helvetica',
+        zIndex: '9999',
+        borderRadius: '16px',
+        color: 'white',
+        textAlign: 'center',
+        display: 'block',
+        backgroundColor: 'rgba(0,0,0, 0.8)'
+      }
+    },
+    logLevel: 'silent',
+    browser: 'google chrome'
+  });
 
-    console.clear();
-    console.log('Starting DevelopmentServer');
-    console.log('           ');
-    console.log('           ');
-    console.log('    /   /  _  / \\     NWA');
-    console.log('   / \\ / \\/ \\/ - \\    Local Development Environment');
-    console.log('           ');
-    console.log('           ');
-    console.log('Listening on port 3000');
-    console.log(messages.jekyllBuild)
-    console.log('           ');
-    console.log('           ');
+  console.clear();
+  console.log('Starting DevelopmentServer');
+  console.log('           ');
+  console.log('           ');
+  console.log('    /   /  _  / \\     NWA');
+  console.log('   / \\ / \\/ \\/ - \\    Local Development Environment');
+  console.log('           ');
+  console.log('           ');
+  console.log('Listening on port 3000');
+  console.log('           ');
+  console.log('           ');
+});
+
+
+// Re-build entire site with clean folders
+gulp.task('prod', ['clean'], () => {
+  gulp.start(['']);
 });
 
 
 // Watcher task
 gulp.task('watch', () => {
-  gulp.watch(path.html, ['html']);
   gulp.watch(path.css, ['css']);
   gulp.watch(path.js, ['js']);
   gulp.watch(path.images, ['imagemin']);
+  gulp.watch(path.jekyll, ['jekyll-rebuild']);
 });
 
 
 // Default task
 gulp.task('default', ['clean'], () => {
-  gulp.start(['html', 'js', 'imagemin', 'server']);
+  gulp.start(['server', 'watch']);
 });
